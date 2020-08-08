76c6da73d000d22ecd47f116b73127184deb7c5e
braydentang1 <brayden.tang1@gmail.com>
2020-08-08 15:39:24 -0600

    fix bugs in layout.

--- nhl_lv/ui.R
+++ nhl_lv/ui.R
@@ -3,18 +3,21 @@ library(shiny)
 # Define UI for application that draws a histogram
 ui <- htmlTemplate(
     "www/index.html",
-    dist_plot = sidebarLayout(
-    	sidebarPanel(
-    		sliderInput("bins",
-    								"Number of bins:",
-    								min = 1,
-    								max = 50,
-    								value = 30)
-    	),
-    	
+    dist_plot = tabsetPanel(
+      tabPanel(title = "Individual Players",
+        sidebarLayout(
+    	    sidebarPanel(
+    		    sliderInput("bins",
+    								    "Number of bins:",
+    								    min = 1,
+    								    max = 50,
+    								    value = 30)
+    	    ),
     	# Show a plot of the generated distribution
     	mainPanel(
-    		plotOutput("distPlot", width = '1000px')
+    		plotOutput("distPlot", width = '500px')
     	)
     )
+  )
+  )
 )
