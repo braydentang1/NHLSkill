0f88933c9056493dc145b0e3c271da1b73df9237
braydentang1 <brayden.tang1@gmail.com>
2020-08-07 19:58:49 -0600

    finally integrate shiny with the template.

--- nhl_lv/ui.R
+++ nhl_lv/ui.R
@@ -2,5 +2,19 @@ library(shiny)
 
 # Define UI for application that draws a histogram
 ui <- htmlTemplate(
-    "www/index.html"
+    "www/index.html",
+    dist_plot = sidebarLayout(
+    	sidebarPanel(
+    		sliderInput("bins",
+    								"Number of bins:",
+    								min = 1,
+    								max = 50,
+    								value = 30)
+    	),
+    	
+    	# Show a plot of the generated distribution
+    	mainPanel(
+    		plotOutput("distPlot", width = '1000px')
+    	)
+    )
 )
